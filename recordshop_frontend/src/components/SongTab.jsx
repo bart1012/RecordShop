392a73bb3045e3b509538779135bef66cac82b18
bart <bielski.bart1012@gmail.com>
2025-04-04 11:40:17 +0100

    Modify song list in product page

--- recordshop_frontend/src/components/SongTab.jsx
+++ recordshop_frontend/src/components/SongTab.jsx
@@ -1,7 +1,11 @@
-import ShoppingCartContext from "../context/ShoppingCartContext";
 
-export default function SongTab (){
-    return 
-        <></>
+export default function SongTab ({index, song}){
+    return (
+        <div className="border-b">
+            <span className="text-xl font-normal p-2 text-gray-500">{index}.</span>
+            <span className="text-lg font-semibold">{song.name}</span>
+            <span className="text-lg float-right text-gray-500">{song.duration}</span>
+        </div>
+    )
 
 }
--- recordshop_frontend/src/pages/product.jsx
+++ recordshop_frontend/src/pages/product.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import {FetchAlbumByID} from "../scripts/apiService"
 import Button from "../components/Button";
+import SongTab from "../components/SongTab";
 
 
 
@@ -27,19 +28,25 @@ const Product = () => {
             <div className="h-85 w-full overflow-hidden">
                 <img src={data?.imgURL} alt={data?.name + "album cover"}></img>
             </div>
-            <div className="flex flex-col p-4">
-                <h2 className="font-bold text-xl">{data?.name}</h2>
-                {data?.artists.map((artist) => <span>{artist}</span>)}
-                <p>{data?.releaseDate}</p>
+            <div className="flex flex-col p-4 gap-6">
+
+                <div className="grid grid-cols-2 grid-rows-2">
+                    <h1 className="font-semibold text-2xl">{data?.name}</h1>
+                    {data?.artists.map((artist) => <h2 className="text-lg text-gray-500 row-start-2">{artist}</h2>)}
+                    <span className="row-span-2 text-lg font-semibold text-center flex items-center justify-end mr-4">£12.99</span>
+                </div>
+
                 <div className="flex flex-col gap-2 items-center max-w-screen">
                     <Button text="Add To Wishlist"></Button>
                     <Button text="Add To Cart" onClickFunction={() => IncreaseCartQuantity(data)}></Button>
                 </div>
+
                 <div>
                     <ul>
-                        {data?.songs.map((song) => <li key={song.name} className="border-b-1">{song.name}</li>)}
+                        {data?.songs.map((song, i) => <li key={song.name}><SongTab song={song} index={i+1}></SongTab></li>)}
                     </ul>
                 </div>
+
             </div>
          </>
    
