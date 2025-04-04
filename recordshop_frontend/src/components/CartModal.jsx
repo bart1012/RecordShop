f466b6ee2f33bdceff263b9a534e9bc4b716a868
bart <bielski.bart1012@gmail.com>
2025-04-04 14:51:02 +0100

    Add modal popup on add to cart

---
+++ recordshop_frontend/src/components/CartModal.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router"
+import { useState } from "react";
+
+
+
+
+export function CartModal ({album, isActive, toggleFunction}) {
+    return(
+        <div className={"lightbox absolute inset-0 min-h-full w-screen bg-white/50 z-550 " + (isActive ? 'show' : 'hidden')}>
+
+            <div className="modalContent m-5 bg-white relative top-1/4 flex flex-col border border-gray-400">
+
+                <div className="modalHeader w-full flex flex-row bg-green-300">
+                    <button className="float-right p-4" onClick={toggleFunction}>✕</button>
+                    <div className="confirmationMessage p-4 text-green-700">
+                        <p>
+                         {album?.name} has been added to your cart.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="modalBody p-4">
+                    <article className="flex flex-row justify-between">
+                  
+
+                        <div className="flex flex-col pb-4">
+                            <h3 className="font-semibold text-xl">{album?.name}</h3>
+                            <h4 className="text-gray-500 mb-5">{album?.artists[0]}</h4>
+                            
+                            <strong>£12.99</strong>
+                            <strong>Quantity: 1</strong>
+                        </div>
+
+                        <figure>
+                            <img src={album?.imgURL} width={120}></img>
+                        </figure>
+
+                    </article>
+                    <hr className="pb-4"></hr>
+                    <nav>
+                        
+                        <div className="flex flex-wrap justify-between">    
+
+                            <button onClick={toggleFunction} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                Continue Shopping
+                            </button>
+                            <Link class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/cart">
+                                View Basket
+                            </Link>
+                        
+                        </div>
+
+                    </nav>
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- recordshop_frontend/src/components/SongTab.jsx
+++ recordshop_frontend/src/components/SongTab.jsx
@@ -4,7 +4,7 @@ export default function SongTab ({index, song}){
         <div className="border-b">
             <span className="text-xl font-normal p-2 text-gray-500">{index}.</span>
             <span className="text-lg font-semibold">{song.name}</span>
-            <span className="text-lg float-right text-gray-500">{song.duration}</span>
+            <span className="text-lg float-right text-gray-500">{Math.round(song.duration * 100) / 100}</span>
         </div>
     )
 
