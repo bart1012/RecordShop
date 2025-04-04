ff56b13b85506d8b63d2c14b5b91e67468725803
bart <bielski.bart1012@gmail.com>
2025-04-04 20:42:53 +0100

    Fix product cards in shopping cart

--- recordshop_frontend/src/components/ProductSummary.jsx
+++ recordshop_frontend/src/components/ProductSummary.jsx
@@ -1,14 +1,27 @@
 const ProductSummary = ({album}) => {
     return <>
-        <div className="bg-red-400">
-            <div>
-                <h2>{album?.name}</h2>
+
+        <div className="bg-white border-t border-b pt-4 pb-4">
+            <div className="flex flex-row ">
+                <figure className="pr-6">
+                    <img src={album?.imgURL} width={120}></img>
+                </figure>
+                <div className="flex flex-col pb-4">
+                                <h3 className="font-semibold text-xl">{album?.name}</h3>
+                                <h4 className="text-gray-500 mb-5">{album?.artists[0]}</h4>
+                                
+                                <strong>£12.99</strong>
+                                <span>Quantity: 1</span>
+                </div>
+            
             </div>
-            <div>
-                <button>Delete</button>
-                <button>Save for later</button>
+            <div className="gap-4 flex flex-row"> 
+                <button className="underline">Delete</button>
+                <button className="underline">Save for later</button>
             </div>
+         
         </div>
+     
     </>
 }
 
--- recordshop_frontend/src/pages/shoppingCart.jsx
+++ recordshop_frontend/src/pages/shoppingCart.jsx
@@ -7,10 +7,19 @@ const ShoppingCart = () => {
     const {cart} = UseShoppingCart();
 
     return <>
-        {cart.map((item) => <ProductSummary album={item}></ProductSummary>)}
-        <br></br>
-        <p>Total: </p>
-        <button>Place order</button>
+        <div className="p-6">
+            <div className="mb-4">
+                <h1 className="text-2xl font-semibold w-full text-center mb-2">Cart</h1>
+                <div className="flex flex-row justify-center">
+                    <span className="itemCount">{cart.length} Items |</span>
+                    <span className="totalPrice ml-2">£0.00</span>
+                </div>
+            </div>
+            {cart.map((item) => <ProductSummary album={item}></ProductSummary>)}
+            <br></br>
+            <p>Total: </p>
+            <button>Place order</button>
+        </div>
     </>
 }
 
